8fa68c8159b5a3fac745edde236e4321f428fb4a
Catherine <catheneghan@gmail.com>
2021-09-02 14:55:01 -0700

    modularized code, seperated api routes

---
+++ db/connection.js
@@ -0,0 +1,13 @@
+const mysql = require('mysql2');
+
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        user: 'root',
+        password: '',
+        database: 'election'
+    },
+    console.log('Connected to the databse')
+);
+
+module.exports = db;
